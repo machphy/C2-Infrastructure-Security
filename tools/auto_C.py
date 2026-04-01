3a5779d9ae00b2aa6f241454883d5888ba516022
machphy <rajeevsharmamachphy@gmail.com>
2026-04-01 14:55:05 +0530

    Auto commit 1

---
+++ tools/auto_C.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+# file jisme changes karne hain
+file_name = "new.txt"
+
+for i in range(1, 11):
+    # file me change add karo
+    with open(file_name, "a") as f:
+        f.write(f"Commit number {i}\n")
+
+    # git add + commit
+    os.system("git add .")
+    os.system(f'git commit -m "Auto commit {i}"')
+
+    print(f"Commit {i} done")
+
+    time.sleep(1)  # optional delay
